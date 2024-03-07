379a7fb76713d9f3b85e4ba635aaab2806da2d50
jing12345678910 <onetwo12826@gmail.com>
2024-03-07 21:25:57 +0800

    forum 240307 增加mock假資料 & 重新規劃App.js的layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,85 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import { Layout, Flex } from "antd";
-import CustomHeader from "./components/Header";
-import CustomContent from "./components/Content";
-import CustomFooter from "./components/Footer";
+import "./App.css";
 
-const { Header, Content, Footer } = Layout;
-
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: "hidden",
-  width: "calc(100% - 8px)",
-  maxWidth: "calc(100% - 8px)",
+const Header = () => {
+  return <div className="header_div">Header</div>;
 };
-const App = () => (
-  <Flex gap="middle" wrap="wrap">
-    <Layout style={layoutStyle}>
-      <Header>
-        <CustomHeader />
-      </Header>
-      <Content>
-        <CustomContent />
-      </Content>
-    </Layout>
-  </Flex>
-);
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar_div">
+      Sidebar
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <main>
+      <p>123</p>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return <div className="footer_div">Footer</div>;
+};
+
+const App = ({ children, className, type }) => {
+  return (
+    <div
+      className={[
+        "layout_wrapper ",
+        type === "header_top" ? "header_top" : "",
+        type === "sidebar_right" ? "sidebar_right" : "",
+        className,
+      ].join(" ")}
+    >
+      {type === "header_top" ? <Header /> : <Sidebar />}
+      <div className="main">
+        {type === "header_top" ? <Sidebar /> : <Header />}
+        <div className="content">
+          <Content />
+        </div>
+        {type === "header_top" ? null : <Footer />}
+      </div>
+      {type === "header_top" ? <Footer /> : null}
+    </div>
+  );
+};
+
 export default App;
--- src/components/BriefArticle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BriefArticle = () => {
-  return (
-    <div>BriefArticle</div>
-  )
-}
-
-export default BriefArticle
